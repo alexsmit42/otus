18c63ae34084e54137875cc30b42ffb5d888c8c8
kuznetsov.al <avalk87@gmail.com>
2023-07-13 16:31:20 +0200

    listener for exceptions

---
+++ src/EventListener/HttpExceptionListener.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\EventListener;
+
+use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\Validator\Exception\ValidationFailedException;
+
+#[AsEventListener(event: ExceptionEvent::class)]
+class HttpExceptionListener
+{
+    public function onKernelException(ExceptionEvent $event): void
+    {
+        $exception = $event->getThrowable();
+
+        if (
+            $exception instanceof HttpException
+            //&& $exception->getPrevious() instanceof ValidationFailedException
+        ) {
+            $event->setResponse(
+                new JsonResponse(
+                    ['success' => false, 'error' => $exception->getMessage()],
+                    $exception->getStatusCode()
+                )
+            );
+        }
+    }
+}
